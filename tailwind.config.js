49deb8eb6537d8e0c1300efca54f9e1b521acf2c
akansson <ahmetakan95@gmail.com>
2024-12-06 16:06:56 +0100

    final

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
